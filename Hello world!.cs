93f81220132b356ba85b018ad1049e8d1c9f6f6b
Daniscoding-somec <dani-pro12@hotmail.com>
2025-01-16 19:52:49 +0100

    Add files via upload

---
+++ Hello world!.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    void Start()
+    {
+        print("Helloworld");
+    }
+    void Update()
+    {
+    
+    }
+}
